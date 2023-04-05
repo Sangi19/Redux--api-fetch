35b476edc462256ad61fd6228975051d1b142cfc
sangeethaRamkumar <sangiammu1020@gmail.com>
2023-04-05 19:50:23 +0530

    datagrid add

--- src/features/restaurant/menus/Menus.js
+++ src/features/restaurant/menus/Menus.js
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {useGetMenusQuery} from '../api/menusApi'
+import { DataGrid } from '@mui/x-data-grid';
 
+
+const columns= [
+    { field: 'name', headerName: 'name', width: 150 },
+    { field: 'tagline', headerName: 'tagline', width: 150 },
+    { field: 'description', headerName: 'description', width: 150 },
+
+
+  ];
+
+//   let rows=data.map((item)=> (
+//     <tr>
+        
+// <td></td>
+//     </tr>
+//   ))
+
+//   let data=[
+//     { id: 1, name: 'Hello', tagline: 'World', description:'xxxxxxxxxxx',xy:"we" },
+//     { id: 2, name: 'DataGridPro', tagline: 'is Awesome',  description:'yyyyyyyyyyy',xy:"we"  },
+//     { id: 3, name: 'MUI', tagline: 'is Amazing', description:'zzzzzzzzzz',xy:"we"  },
+//   ]
 export default function Menus() {
 
-// const [drinks, setDrinks] = useState(data)
-
-    const { data } = useGetMenusQuery(1);
-
+    const { data,isLoading } = useGetMenusQuery(1);
+    
+    const [drinks, setDrinks] = useState(data)
     console.log(data)
+
+    useEffect(()=>{
+        
+    }
+    ,[drinks,data])
+
   return (
     <div>
         <input placeholder='Enter the name to filter'/>
+        {isLoading ? (
+        <div>Loading....</div>
+      ) : (
         <div  style={{ height: 300, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} />
+            <DataGrid rows={drinks} columns={columns} />
             <h1>pagination</h1>
         </div>
+    )}
     </div>
   )
 }
